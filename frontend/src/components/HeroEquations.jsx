5601d3d49ee9c5f1d0383acac70f6bc4a46c3f91
YashIsTheBest247 <yashmunshi27@gmail.com>
2026-06-20 18:49:53 +0530

    changed background equation opacity

--- frontend/src/components/HeroEquations.jsx
+++ frontend/src/components/HeroEquations.jsx
@@ -9,6 +9,13 @@ const EQUATIONS = [
   { tex: "\\nabla\\cdot\\vec{E}=\\frac{\\rho}{\\varepsilon_0}", top: 13, left: 56, size: 24, rot: 6, dur: 17, delay: 1.5 },
   { tex: "e^{i\\pi}+1=0", top: 72, left: 62, size: 30, rot: -5, dur: 19, delay: 2.5 },
   { tex: "\\lim_{x\\to0}\\frac{\\sin x}{x}=1", top: 50, left: 26, size: 25, rot: 3, dur: 20, delay: 0.8 },
+  { tex: "\\binom{n}{k}=\\frac{n!}{k!(n-k)!}", top: 8, left: 22, size: 23, rot: -4, dur: 22, delay: 1.2 },
+  { tex: "\\oint_C \\vec{F}\\cdot d\\vec{r}", top: 30, left: 70, size: 26, rot: 4, dur: 18, delay: 0.4 },
+  { tex: "\\theta=\\tan^{-1}\\frac{y}{x}", top: 90, left: 60, size: 24, rot: -3, dur: 24, delay: 2.2 },
+  { tex: "\\frac{\\partial^2 u}{\\partial t^2}=c^2\\nabla^2 u", top: 56, left: 50, size: 22, rot: 5, dur: 20, delay: 1.8 },
+  { tex: "\\det(A-\\lambda I)=0", top: 78, left: 28, size: 25, rot: -5, dur: 21, delay: 0.6 },
+  { tex: "\\sqrt{b^2-4ac}", top: 44, left: 12, size: 27, rot: 4, dur: 19, delay: 2.8 },
+  { tex: "P(A\\mid B)=\\frac{P(B\\mid A)P(A)}{P(B)}", top: 24, left: 36, size: 21, rot: -2, dur: 23, delay: 1.4 },
 ];
 
 function Equation({ tex }) {
